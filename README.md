c6e7b4ff7d5c3cbc730e499940cd65a0849c1bf3
DublikuntMux <dublikunt@gmail.com>
2023-11-15 15:05:21 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Skia
+
+## Info
+
+Skia is a complete 2D graphic library for drawing Text, Geometries, and Images.  
+See full details, and build instructions, at <https://skia.org>.
+
+## Cmake
+
+As release use chrome/m120 branch.
---
+++ include/core/SkCapabilities.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef SkCapabilities_DEFINED
+#define SkCapabilities_DEFINED
+
+#include "include/core/SkRefCnt.h"
+#include "include/core/SkTypes.h"
+#include "include/sksl/SkSLVersion.h"
+
+namespace SkSL { struct ShaderCaps; }
+
+#if defined(SK_GRAPHITE)
+namespace skgpu::graphite { class Caps; }
+#endif
+
+class SK_API SkCapabilities : public SkRefCnt {
+public:
+    static sk_sp<const SkCapabilities> RasterBackend();
+
+    SkSL::Version skslVersion() const { return fSkSLVersion; }
+
+    void initSkCaps(const SkSL::ShaderCaps*);
+
+    SkCapabilities() = default;
+
+protected:
+#if defined(SK_GRAPHITE)
+    friend class skgpu::graphite::Caps; // for ctor
+#endif
+    SkSL::Version fSkSLVersion = SkSL::Version::k100;
+};
+
+#endif
